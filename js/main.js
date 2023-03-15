c0d43c48d58d5148682c9695a424a93d644761b7
LucaBrivio183 <l.brivio4@gmail.com>
2023-03-15 16:34:40 +0100

    feat:  MILESTONE 1

--- js/main.js
+++ js/main.js
@@ -3,13 +3,39 @@ const  { createApp } = Vue; //initialize Vue
 createApp ({
     data() { 
         return {            
-            
+            pageTitle:'Vue To Do List',
+            toDoList: [
+                {
+                    text:'prepare pancake',
+                    done: true,
+                },{
+                    text:'take supplements with salted water',
+                    done: false,
+                },{
+                    text:'sunlight',
+                    done: false,
+                },{
+                    text:'do focus games',
+                    done: false,
+                },{
+                    text:'read',
+                    done: false,
+                },{
+                    text:'do bed',
+                    done: false,
+                },{
+                    text:'meditate',
+                    done: false,
+                },{
+                    text:'start working',
+                    done: false,
+                }
+            ],
         }
     }, 
     methods: {
     
-    },
-    
+    },   
 }).mount('#app');
 
 
